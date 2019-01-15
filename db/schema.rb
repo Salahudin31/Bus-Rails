b1712539de32d23d1ee785430724e51d9f2205bd
Salahudin31 <salahudin.310795@gmail.com>
2019-01-16 01:36:24 +0700

    Bus Rails Api

--- db/schema.rb
+++ db/schema.rb
@@ -12,18 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2019_01_08_123904) do
 
-  create_table "bangkus", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "bangkus", force: :cascade do |t|
     t.integer "no_bangku"
     t.string "keterangan"
-    t.bigint "bus_id"
-    t.bigint "jadwal_id"
+    t.integer "bus_id"
+    t.integer "jadwal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bus_id"], name: "index_bangkus_on_bus_id"
     t.index ["jadwal_id"], name: "index_bangkus_on_jadwal_id"
   end
 
-  create_table "buses", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "buses", force: :cascade do |t|
     t.string "no_bus"
     t.string "jenis_bus"
     t.integer "jumlah_b"
@@ -32,40 +32,40 @@ ActiveRecord::Schema.define(version: 2019_01_08_123904) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "jadwals", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "jadwals", force: :cascade do |t|
     t.date "tgl_b"
     t.time "jam_b"
     t.string "status_bangku"
-    t.bigint "bus_id"
-    t.bigint "rute_id"
+    t.integer "bus_id"
+    t.integer "rute_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bus_id"], name: "index_jadwals_on_bus_id"
-    t.index ["rute_id"], name: "rute_id"
+    t.index ["rute_id"], name: "index_jadwals_on_rute_id"
   end
 
-  create_table "pesanan_tikets", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "pesanan_tikets", force: :cascade do |t|
     t.string "nama_pen"
     t.integer "no_telp"
     t.string "alamat"
     t.string "status_pem"
     t.integer "jmlh_pem"
-    t.bigint "bangku_id"
-    t.bigint "jadwal_id"
+    t.integer "bangku_id"
+    t.integer "jadwal_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["bangku_id"], name: "index_pesanan_tikets_on_bangku_id"
     t.index ["jadwal_id"], name: "index_pesanan_tikets_on_jadwal_id"
   end
 
-  create_table "rutes", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "rutes", force: :cascade do |t|
     t.string "start"
     t.string "end"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
     t.string "email"
@@ -74,9 +74,4 @@ ActiveRecord::Schema.define(version: 2019_01_08_123904) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "bangkus", "buses"
-  add_foreign_key "bangkus", "jadwals"
-  add_foreign_key "jadwals", "buses"
-  add_foreign_key "pesanan_tikets", "bangkus"
-  add_foreign_key "pesanan_tikets", "jadwals"
 end
